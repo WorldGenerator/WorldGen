dc731634b9ba77950ede9742ae4a28129580cd52
tmjchu <tmj.chu@berkeley.edu>
2018-04-14 22:51:22 -0700

    updates - land,world

--- Ecology/src/Land.java
+++ Ecology/src/Land.java
@@ -1,21 +1,26 @@
+import java.util.List;
+import java.util.ArrayList;
+
+
 public class Land {
 	private final Coordinate loc;
 	private boolean plant = false;
-	private List<Rabbit>[] rabbits;
-	private List<Fox>[] foxy;
+	private List<Animal> rabbits;
+	private List<Animal> foxy;
 
 	public Land(int x, int y) {
 		loc  = new Coordinate(x,y);
-		container = new ArrayList();
+		rabbits = new ArrayList<Animal>();
+		foxy = new ArrayList<Animal>();
 	}
 
-	public void insert(Object target) {
-		if (target instanceof Rabbit) {
-			rabbits.add(target);
-		} else if (target instanceof Fox) {
-			foxy.add(target);
-		} else {
-			System.out.println("Should not be added");
-		}
+	public void insert(Animal target) {
+		// if (target instanceof Rabbit) {
+		// 	rabbits.add(target);
+		// } else if (target instanceof Fox) {
+		// 	foxy.add(target);
+		// } else {
+		// 	System.out.println("Should not be added");
+		// }
 	}
 }
--- Ecology/src/World.java
+++ Ecology/src/World.java
@@ -1,7 +1,22 @@
 public class World {
-	private Land[][] theWorld = new Land[50][50];
+	private Land[][] theWorld = null;
 	private int plants;
 	private int fox;
 	private int rabbit;
 
+	public World(int x, int y) {
+		theWorld = new Land[x][y];
+
+		int i = 0;
+		int size = x*y;
+		while (i < size) {
+			theWorld[(int)i/y][i%y] = new Land((int)i/y, i%y);
+			i += 1;
+
+		}
+	}
+
+	public static void main(String[] args) {
+		World n = new World(50, 50);
+	}
 }
